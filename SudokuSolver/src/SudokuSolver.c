0e0c27411972c0967a2c0376ae228c1ffd0c479b
rkamae <b9122014@edu.kit.ac.jp>
2020-01-09 18:49:06 +0900

    コーディング規則に合わせて修正

--- SudokuSolver/src/SudokuSolver.c
+++ SudokuSolver/src/SudokuSolver.c
@@ -14,84 +14,98 @@
 #define OVERALL_SIZE 9
 #define BOX_SIZE 3
 
-void display(int *board){
+void display(int *board)
+{
 	int i;
-	for(i = 0;i < OVERALL_SIZE * OVERALL_SIZE;i++){
+	for(i = 0; i < OVERALL_SIZE * OVERALL_SIZE; i++)
+	{
 		printf("%d", board[i]);
-//		if(i % BOX_SIZE == BOX_SIZE - 1){
-//			printf(" ");
-//		}
-		if(i % OVERALL_SIZE == OVERALL_SIZE - 1){
+		if(i % OVERALL_SIZE == OVERALL_SIZE - 1)
+		{
 			printf("\n");
 		}
 	}
 }
 
-int canPut(int *board, int place, int number){
-	int row = place / OVERALL_SIZE;
-	int column = place % OVERALL_SIZE;
-	int box = (row / BOX_SIZE) * BOX_SIZE * OVERALL_SIZE + (column / BOX_SIZE) * BOX_SIZE;
+int canPut(int *board, int place, int number)
+{
+	int row 	= place / OVERALL_SIZE;
+	int column 	= place % OVERALL_SIZE;
+	int box 	= ((row / BOX_SIZE) * BOX_SIZE * OVERALL_SIZE) + ((column / BOX_SIZE) * BOX_SIZE);
 	int i;
-	for(i = 0;i < OVERALL_SIZE;i++){
-		if(board[row * OVERALL_SIZE + i] == number){
+	for(i = 0; i < OVERALL_SIZE; i++)
+	{
+		if(board[(row * OVERALL_SIZE) + i] == number)
+		{
 			return 0;
 		}
-		if(board[column + i * OVERALL_SIZE] == number){
+		if(board[column + (i * OVERALL_SIZE)] == number)
+		{
 			return 0;
 		}
-		if(board[box + (i / BOX_SIZE) * OVERALL_SIZE + (i % BOX_SIZE)] == number){
+		if(board[box + ((i / BOX_SIZE) * OVERALL_SIZE) + (i % BOX_SIZE)] == number)
+		{
 			return 0;
 		}
 	}
 	return 1;
 }
 
-int solve(int *board, int place){
-	if(place == OVERALL_SIZE * OVERALL_SIZE){
+int solve(int *board, int place)
+{
+	if(place == OVERALL_SIZE * OVERALL_SIZE)
+	{
 		return 1;
-		// display(board);
-		// printf("\n");
 	}
 
 	int newPlace;
-	for(newPlace = place;newPlace < OVERALL_SIZE * OVERALL_SIZE;newPlace++){
-		if(!board[newPlace]){
+	for(newPlace = place; newPlace < OVERALL_SIZE * OVERALL_SIZE; newPlace++)
+	{
+		if(!board[newPlace])
+		{
 			break;
 		}
 	}
 
 	int i;
-	for(i = 1;i <= OVERALL_SIZE;i++){
-		if(canPut(board, newPlace, i)){
+	for(i = 1; i <= OVERALL_SIZE; i++)
+	{
+		if(canPut(board, newPlace, i))
+		{
 			board[newPlace] = i;
-			if(solve(board, newPlace + 1)){
+			if(solve(board, newPlace + 1))
+			{
 				return 1;
 			}
-			// solve(board, newPlace + 1);
 			board[newPlace] = 0;
 		}
 	}
 	return 0;
 }
 
-int main() {
+int main() 
+{
 	char input[OVERALL_SIZE][OVERALL_SIZE];
 	int i;
-	for(i = 0;i < OVERALL_SIZE;i++){
+	for(i = 0; i < OVERALL_SIZE; i++)
+	{
 		scanf("%s", input[i]);
 	}
 	printf("\n");
 	int board[OVERALL_SIZE * OVERALL_SIZE];
-	for(i = 0; i < OVERALL_SIZE * OVERALL_SIZE;i++){
+	for(i = 0; i < OVERALL_SIZE * OVERALL_SIZE; i++)
+	{
 		board[i] = input[i / OVERALL_SIZE][i % OVERALL_SIZE] - '0';
 	}
-	if(solve(board, 0)){
+	if(solve(board, 0))
+	{
 		display(board);
 		printf("This problem can be solved.\n");
-	}else{
+	}
+	else
+	{
 		printf("This problem can NOT be solved.\n");
 	}
-	// solve(board, 0);
 	return 0;
 }
 
